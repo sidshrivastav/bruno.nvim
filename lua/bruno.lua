20189a593a6b998270d81cbf7d850b93cade801c
sidshrivastav <siddhantshrivastava@outlook.com>
2024-01-24 13:00:53 +0530

    feat: added command to run bru file

--- lua/bruno.lua
+++ lua/bruno.lua
@@ -1,2 +1,9 @@
-print("Hello from our plugin")
+function! RunBruCommand(...)
+    let command = 'bru ' . a:1
+    for arg in a:000[1:]
+        let command .= ' ' . arg
+    endfor
+
+" Define a command to run Bru commands "
+command! -nargs=* bru call RunBruCommand(<f-args>)
 
